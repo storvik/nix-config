d5a8b99c8c5ed65ccc5508e8dd848750ef80610a
storvik <petterstorvik@gmail.com>
2020-11-23 06:07:51 +0100

    Use flutter from unstable

--- home-manager/profiles/developer/android.nix
+++ home-manager/profiles/developer/android.nix
@@ -1,14 +1,9 @@
 { config, pkgs, lib, ... }:
-let
-
-  flutterPull = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/cadfab01bd7908322036147c995d1146f5d604bc.tar.gz) { config = config.nixpkgs.config; };
-
-in
 {
 
   home.packages = with pkgs; [
     android-studio
-    flutterPull.flutterPackages.stable
+    flutterPackages.stable
     scrcpy
   ];
 
