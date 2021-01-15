348212c5ba6364656e8974f4bfd11059724ccf99
storvik <petterstorvik@gmail.com>
2021-01-15 21:21:20 +0100

    Add powershell to developer

--- home-manager/developer.nix
+++ home-manager/developer.nix
@@ -9,13 +9,9 @@
     ./developer/lisp.nix
     ./developer/nix.nix
     ./developer/python.nix
+    ./developer/powershell.nix
     ./developer/webdev.nix
   ];
-
-  # Try lorri https://github.com/target/lorri
-  # services.lorri.enable = true;
-  # Decided to try nix-direnv first https://github.com/nix-community/nix-direnv
-  # This mostly due to the lorri daemon
 
   programs.direnv = {
     enable = true;
