43474e0bffa9722e6fb8de6319a174c2cb0b30f5
storvik <petterstorvik@gmail.com>
2021-05-17 21:13:42 +0200

    android: add platform tools

--- modules/home-manager/developer/android.nix
+++ modules/home-manager/developer/android.nix
@@ -2,14 +2,21 @@
 
 with lib;
 
+let
+
+  android = pkgs.androidenv.composeAndroidPackages { };
+
+in
+
 {
 
   config = mkIf (config.storvik.developer.android.enable || config.storvik.developer.enable)
     {
       home.packages = with pkgs; [
+        android.platform-tools
         apktool
-        scrcpy
         dart
+        scrcpy
       ];
     };
 
