d63c9591bcffeb044aebb8b67f5fdd8f4722b2ee
storvik <petterstorvik@gmail.com>
2022-01-23 21:30:55 +0100

    alacritty: use nord theme

--- modules/home-manager/shell.nix
+++ modules/home-manager/shell.nix
@@ -128,22 +128,69 @@ in
         window = {
           decorations = "None";
         };
-        # Colors (Doom One)
-        # https://github.com/eendroroy/alacritty-theme/blob/master/themes/doom_one.yml
+        # Project:    Nord Alacritty
+        # Version:    0.1.0
+        # Repository: https://github.com/arcticicestudio/nord-alacritty
+        # License:    MIT
+        # References:
+        #   https://github.com/alacritty/alacritty
         colors = {
           primary = {
-            background = "0x282c34";
-            foreground = "0xbbc2cf";
+            background = "#2e3440";
+            foreground = "#d8dee9";
+            dim_foreground = "#a5abb6";
+          };
+          cursor = {
+            text = "#2e3440";
+            cursor = "#d8dee9";
+          };
+          vi_mode_cursor = {
+            text = "#2e3440";
+            cursor = "#d8dee9";
+          };
+          selection = {
+            text = "CellForeground";
+            background = "#4c566a";
+          };
+          search = {
+            matches = {
+              foreground = "CellBackground";
+              background = "#88c0d0";
+            };
+            bar = {
+              background = "#434c5e";
+              foreground = "#d8dee9";
+            };
           };
           normal = {
-            black = "0x282c34";
-            red = "0xff6c6b";
-            green = "0x98be65";
-            yellow = "0xecbe7b";
-            blue = "0x51afef";
-            magenta = "0xc678dd";
-            cyan = "0x46d9ff";
-            white = "0xbbc2cf";
+            black = "#3b4252";
+            red = "#bf616a";
+            green = "#a3be8c";
+            yellow = "#ebcb8b";
+            blue = "#81a1c1";
+            magenta = "#b48ead";
+            cyan = "#88c0d0";
+            white = "#e5e9f0";
+          };
+          bright = {
+            black = "#4c566a";
+            red = "#bf616a";
+            green = "#a3be8c";
+            yellow = "#ebcb8b";
+            blue = "#81a1c1";
+            magenta = "#b48ead";
+            cyan = "#8fbcbb";
+            white = "#eceff4";
+          };
+          dim = {
+            black = "#373e4d";
+            red = "#94545d";
+            green = "#809575";
+            yellow = "#b29e75";
+            blue = "#68809a";
+            magenta = "#8c738c";
+            cyan = "#6d96a5";
+            white = "#aeb3bb";
           };
         };
         font = {
@@ -165,7 +212,6 @@ in
       bitwarden-cli
       bfg-repo-cleaner
       ltunify
-      nmap
       pdftk
       rclone
       storvik-backup
