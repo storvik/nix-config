1858ce7b614c12b0c4e6c82bb90ade99373b0793
storvik <petterstorvik@gmail.com>
2022-01-23 21:30:33 +0100

    nixos: add debootstrap

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -13,4 +13,8 @@
     binaryCachePublicKeys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
   };
 
+  environment.systemPackages = with pkgs; [
+    debootstrap
+  ];
+
 }
