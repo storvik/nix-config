1ac7fe78363b678ec0200f5335aee684eb672b26
storvik <petterstorvik@gmail.com>
2022-06-11 19:43:21 +0200

    emacs: rename emacsPgtkGcc to emacsPgtkNativeComp

--- modules/home-manager/emacs.nix
+++ modules/home-manager/emacs.nix
@@ -8,7 +8,7 @@ with lib;
 
     programs.emacs = {
       enable = true;
-      package = if config.storvik.emacs.nativeComp then pkgs.emacsPgtkGcc else pkgs.emacsPgtk;
+      package = if config.storvik.emacs.nativeComp then pkgs.emacsPgtkNativeComp else pkgs.emacsPgtk;
     };
 
     home.sessionVariables = {
