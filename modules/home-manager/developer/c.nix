00afb4b1ac50db62ee502e357bffd693479f0144
storvik <petterstorvik@gmail.com>
2022-06-17 11:25:56 +0200

    developer / c: remove ccls

--- modules/home-manager/developer/c.nix
+++ modules/home-manager/developer/c.nix
@@ -7,7 +7,6 @@ with lib;
   config = mkIf (config.storvik.developer.c.enable || config.storvik.developer.enable)
     {
       home.packages = with pkgs; [
-        ccls
         clang-tools
         cmake
         gdb
