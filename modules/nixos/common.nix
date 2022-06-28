e2cf885a09b6fa3552c3efddfd3265ddb2e9de94
storvik <petterstorvik@gmail.com>
2022-06-28 20:45:34 +0200

    common: add some localization options to common

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -13,6 +13,14 @@
     binaryCachePublicKeys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
   };
 
+  console.keyMap = "no";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Set your time zone.
+  time.timeZone = "Europe/Oslo";
+
   environment.systemPackages = with pkgs; [
     debootstrap
   ];
--- modules/nixos/desktops/gnome.nix
+++ modules/nixos/desktops/gnome.nix
@@ -9,16 +9,6 @@ with lib;
     # Enable network manager
     networking.networkmanager.enable = true;
 
-    # Select internationalisation properties.
-    i18n.defaultLocale = "en_US.UTF-8";
-    console = {
-      font = "Lat2-Terminus16";
-      keyMap = "no";
-    };
-
-    # Set your time zone.
-    time.timeZone = "Europe/Oslo";
-
     services = {
 
       # Fix for dconf
