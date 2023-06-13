7da1febcd73450bee0a4ef2f9a97965c0306efe3
storvik <petterstorvik@gmail.com>
2023-06-13 10:47:36 +0200

    android: remove tasker permission script

--- modules/home-manager/developer/android.nix
+++ modules/home-manager/developer/android.nix
@@ -6,36 +6,6 @@ let
 
   android = pkgs.androidenv.composeAndroidPackages { };
 
-  tasker-permissions = [
-    "DUMP" # check current running services (Context - Application)
-    "SET_MEDIA_KEY_LISTENER" # media key press handling (Context - State - Media Button)
-    "READ_LOGS" # logacat entries (Context - Event - LogCat Entry)
-    "SET_VOLUME_KEY_LONG_PRESS_LISTENER" # colume key long press handling (Context - Event - Hardware - Volume Long Press)
-    "WRITE_SECURE_SETTINGS" # write secure settings (Action - Settings - Custom Setting)
-    "CHANGE_CONFIGURATION" # change system locale
-    "SYSTEM_ALERT_WINDOW" # draw on other apps
-    "PACKAGE_USAGE_STATS" # react to app changes and get app info / stats
-    "ANSWER_PHONE_CALLS" # answer phone calls
-    "CALL_PHONE" # make phone calls
-  ];
-
-  storvik-grant-android-permissions = android-package: permissions: (
-    strings.concatMapStrings (x: "adb shell pm grant " + android-package + " android.permission." + x + "\n") permissions
-  );
-
-  storvik-grant-tasker = pkgs.writeScriptBin "storvik-grant-tasker" ''
-    #!${pkgs.bash}/bin/bash
-
-    ${storvik-grant-android-permissions "net.dinglisch.android.taskerm"  tasker-permissions}
-
-    # restart Tasker
-    adb shell am force-stop net.dinglisch.android.taskerm
-
-
-    # enable adb wifi
-    adb tcpip 5555
-  '';
-
 in
 
 {
@@ -46,7 +16,6 @@ in
         android.platform-tools
         apktool
         scrcpy
-        storvik-grant-tasker
       ];
     };
 
