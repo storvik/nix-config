a2b328fd8e3f99a27e86114941d0193b943e1a37
storvik <petterstorvik@gmail.com>
2023-07-06 22:13:17 +0200

    git: add git cliff

--- modules/home-manager/users/storvik.nix
+++ modules/home-manager/users/storvik.nix
@@ -25,6 +25,12 @@ with lib;
         ".direnv"
         "result"
       ];
+    programs.git-cliff = {
+      enable = true;
+      settings = {
+        header = "Changelog";
+        trim = true;
+      };
     };
 
     home.packages =
