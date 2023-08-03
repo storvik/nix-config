bcc32418697f688b1f1abf0aa86211c454bf9aa6
storvik <petterstorvik@gmail.com>
2023-08-03 12:51:03 +0200

    gpg: use qt version

--- modules/home-manager/users/storvik.nix
+++ modules/home-manager/users/storvik.nix
@@ -152,7 +152,7 @@ with lib;
       enable = true;
       enableBashIntegration = true;
       enableFishIntegration = true;
-      pinentryFlavor = if config.storvik.wsl.enable then null else "gtk2"; # When set to null it does not set pinentry-program in conf
+      pinentryFlavor = if config.storvik.wsl.enable then null else "qt"; # When set to null it does not set pinentry-program in conf
       extraConfig = if config.storvik.wsl.enable then "pinentry-program /mnt/c/Users/petter.storvik/scoop/apps/gpg4win/current/Gpg4win/bin/pinentry.exe" else "allow-loopback-pinentry";
     };
 
