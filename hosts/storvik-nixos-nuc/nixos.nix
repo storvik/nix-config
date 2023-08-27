ce3ebe6513420b589ad9797aa5f7318f7bd1c29d
storvik <petterstorvik@gmail.com>
2023-08-27 17:33:40 +0200

    nuc: add public ssh keys

--- hosts/storvik-nixos-nuc/nixos.nix
+++ hosts/storvik-nixos-nuc/nixos.nix
@@ -1,6 +1,14 @@
 { pkgs, ... }:
 
 {
+
+  users.users.storvik = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG/cetz89/SRWucBZPsARH8pnHwXCW9MGrHmNJyhHMCC petterstorvik@gmail.com" # matebook
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBoFq88oaivkC4bqCUINV6DRwg6Qfkd+a8gC6Mc68EKB petter.storvik@goodtech.no" # lenovo
+    ];
+  };
+
   services.syncthing = {
     enable = true;
     dataDir = "/home/storvik";
