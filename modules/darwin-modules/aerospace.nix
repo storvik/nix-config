e4f14f9177566095cd236450e038fa85b3a846fa
storvik <petterstorvik@gmail.com>
2025-01-25 13:26:11 +0100

    aerospace: use ctrl instead of alt

--- modules/darwin-modules/aerospace.nix
+++ modules/darwin-modules/aerospace.nix
@@ -27,7 +27,7 @@ in
           on-focused-monitor-changed = [ "move-mouse monitor-lazy-center" ];
           on-focus-changed = [ "move-mouse window-lazy-center" ];
           workspace-to-monitor-force-assignment = {
-            "P" = [ "^C27F390.*1*$" ];
+            "P" = [ "^C27F390.*1*$" "^HP.*2*$" ];
             "B" = [ "^built-in retina display$" ];
           };
           on-window-detected = [
@@ -40,62 +40,53 @@ in
                 # window-title-regex-substring = "Title";
                 # workspace = "cool-workspace";
               };
-              run = [ "move-node-to-workspace 7" ];
+              run = [ "move-node-to-workspace 3" ];
             }
           ];
           mode.main.binding = {
 
-            alt-enter = "exec-and-forget open -na alacritty";
-            shift-alt-enter = "exec-and-forget ${pkgs.storvik-emacs-withPackages}/bin/emacsclient -c";
+            ctrl-enter = "exec-and-forget open -na alacritty";
+            shift-ctrl-enter = "exec-and-forget ${pkgs.storvik-emacs-withPackages}/bin/emacsclient -c";
 
-            alt-m = "focus left";
-            alt-n = "focus down";
-            alt-e = "focus up";
-            alt-i = "focus right";
+            ctrl-m = "focus left";
+            ctrl-n = "focus down";
+            ctrl-e = "focus up";
+            ctrl-i = "focus right";
 
-            alt-shift-m = "move left";
-            alt-shift-n = "move down";
-            alt-shift-e = "move up";
-            alt-shift-i = "move right";
+            ctrl-shift-m = "move left";
+            ctrl-shift-n = "move down";
+            ctrl-shift-e = "move up";
+            ctrl-shift-i = "move right";
 
-            alt-slash = "layout tiles horizontal vertical";
-            alt-comma = "layout accordion horizontal vertical";
+            ctrl-slash = "layout tiles horizontal vertical";
+            ctrl-comma = "layout accordion horizontal vertical";
 
-            alt-f = "fullscreen";
-            alt-shift-f = "layout floating tiling";
+            ctrl-f = "fullscreen";
+            ctrl-shift-f = "layout floating tiling";
 
-            alt-shift-minus = "resize smart -50";
-            alt-shift-equal = "resize smart +50";
+            # alt-shift-minus = "resize smart -50";
+            # alt-shift-equal = "resize smart +50";
 
-            alt-1 = "workspace 1";
-            alt-2 = "workspace 2";
-            alt-3 = "workspace 3";
-            alt-4 = "workspace 4";
-            alt-5 = "workspace 5";
-            alt-6 = "workspace 6";
-            alt-7 = "workspace 7";
-            alt-8 = "workspace 8";
-            alt-9 = "workspace 9";
-            alt-b = "workspace B";
-            alt-p = "workspace P";
+            ctrl-a = "workspace 5";
+            ctrl-r = "workspace 4";
+            ctrl-s = "workspace 3";
+            ctrl-t = "workspace 2";
+            ctrl-g = "workspace 1";
+            ctrl-b = "workspace B";
+            ctrl-p = "workspace P";
 
-            alt-shift-1 = "move-node-to-workspace 1";
-            alt-shift-2 = "move-node-to-workspace 2";
-            alt-shift-3 = "move-node-to-workspace 3";
-            alt-shift-4 = "move-node-to-workspace 4";
-            alt-shift-5 = "move-node-to-workspace 5";
-            alt-shift-6 = "move-node-to-workspace 6";
-            alt-shift-7 = "move-node-to-workspace 7";
-            alt-shift-8 = "move-node-to-workspace 8";
-            alt-shift-9 = "move-node-to-workspace 9";
-            alt-shift-b = "move-node-to-workspace B";
-            alt-shift-p = "move-node-to-workspace P";
+            ctrl-shift-a = "move-node-to-workspace 5";
+            ctrl-shift-r = "move-node-to-workspace 4";
+            ctrl-shift-s = "move-node-to-workspace 3";
+            ctrl-shift-t = "move-node-to-workspace 2";
+            ctrl-shift-g = "move-node-to-workspace 1";
+            ctrl-shift-b = "move-node-to-workspace B";
+            ctrl-shift-p = "move-node-to-workspace P";
 
-            alt-period = "focus-monitor main";
-            alt-tab = "workspace-back-and-forth";
-            alt-shift-tab = "move-workspace-to-monitor --wrap-around next";
+            ctrl-tab = "workspace-back-and-forth";
+            ctrl-shift-tab = "move-workspace-to-monitor --wrap-around next";
 
-            alt-shift-semicolon = "mode service";
+            ctrl-shift-semicolon = "mode service";
           };
           mode.service.binding = {
             esc = [ "reload-config" "mode main" ];
@@ -104,10 +95,10 @@ in
             # s [= "layout sticky tiling" "mode main"]; # sticky is not yet supported https://github.com/nikitabobko/AeroSpace/issues/2
             backspace = [ "close-all-windows-but-current" "mode main" ];
 
-            alt-shift-m = [ "join-with left" "mode main" ];
-            alt-shift-n = [ "join-with down" "mode main" ];
-            alt-shift-e = [ "join-with up" "mode main" ];
-            alt-shift-i = [ "join-with right" "mode main" ];
+            ctrl-shift-m = [ "join-with left" "mode main" ];
+            ctrl-shift-n = [ "join-with down" "mode main" ];
+            ctrl-shift-e = [ "join-with up" "mode main" ];
+            ctrl-shift-i = [ "join-with right" "mode main" ];
           };
         };
       };
